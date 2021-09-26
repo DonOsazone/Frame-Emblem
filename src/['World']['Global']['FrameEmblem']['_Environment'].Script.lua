d18d0ce687bb0a1edfe0eee90ea56409c1c1da84
Gyss <donosazone@gmail.com>
2021-09-26 19:11:49 +0800

    基础内容&git测试

---
+++ src/['World']['Global']['FrameEmblem']['_Environment'].Script.lua
@@ -0,0 +1,6 @@
+--- script  t_Environment
+--- Created by Gyss
+--- Latest Editted by Gyss
+--- copyRight © 2021   AvatarConstructors
+Debug.EnableLog("iew")
+
---
+++ src/['World']['Global']['FrameEmblem']['_SETTING'].Script.lua
@@ -0,0 +1,8 @@
+--- script  _SETTING
+--- Created by Gyss
+--- Latest Editted by Gyss
+--- copyRight © 2021   AvatarConstructors
+
+_SETTING = {}
+
+_SETTING.example = "example"
